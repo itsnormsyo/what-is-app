e45154525f63dbc20b60273e2100db06b9afe0ea
Norman Fung <60231114+itsnormsyo@users.noreply.github.com>
2020-09-13 19:44:04 -0700

    "first commit"

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwX_fumeF_PGH7Mr3PSBUd52HpcA3gJko",
+  authDomain: "whatsapp-clone-norman.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-norman.firebaseio.com",
+  projectId: "whatsapp-clone-norman",
+  storageBucket: "whatsapp-clone-norman.appspot.com",
+  messagingSenderId: "29093992532",
+  appId: "1:29093992532:web:75b14c56f72552ff65b5c1",
+  measurementId: "G-T82SWZGCEN"
+};
