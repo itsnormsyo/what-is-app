bace470e8b7e4bd4bbacf64c5c84b7e55ec99316
itsnormsyo <itsnormsyo@gmail.com>
2020-09-17 02:31:13 -0700

    finished build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import Sidebar from "./Sidebar";
-import Chat from './Chat';
-import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
-import Login from './Login';
+import Chat from "./Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login";
 import { useStateValue } from "./StateProvider";
 
 function App() {
@@ -12,23 +12,23 @@ function App() {
   return (
     //BEM naming convention
     <div className="app">
-    {!user ? (
-      <Login />
-    ) :  (
-      <div className="app__body">
-      <Router>
-          <Sidebar />
-          <Switch>
-          <Route path="/rooms/:roomId">        
-            <Chat />
-            </Route>
-            <Route path="/">
-            <Chat />
-          </Route>
-         </Switch>
-        </Router>
-      </div>
-    )}
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
--- src/Chat.js
+++ src/Chat.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
 import { useParams } from "react-router-dom";
-import db from './firebase';
+import db from "./firebase";
 import firebase from "firebase";
 import { useStateValue } from "./StateProvider";
+
 
 function Chat() {
   const [input, setInput] = useState("");
@@ -19,19 +20,17 @@ function Chat() {
 
   useEffect(() => {
     if (roomId) {
-      db.collection('rooms')
-      .doc(roomId)
-      .onSnapshot((snapshot) => setRoomName
-      (snapshot.data().name));
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomName(snapshot.data().name));
 
-      db.collection('rooms')
-      .doc(roomId)
-      .collection('messages')
-      .orderBy('timestamp', 'asc')
-      .onSnapshot((snapshot) => 
-        setMessages(snapshot.docs.map((doc) => 
-        doc.data()))
-      );
+      db.collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
     }
   }, [roomId]);
 
@@ -42,64 +41,79 @@ function Chat() {
   const sendMessage = (e) => {
     e.preventDefault();
 
-
-    db.collection("rooms").doc(roomId).collection
-    ("messages").add({
+    db.collection("rooms").doc(roomId).collection("messages").add({
       message: input,
       name: user.displayName,
-      timestamp: firebase.firestore.FieldValue.
-      serverTimestamp(),
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
 
     setInput("");
   };
 
+  const handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    setInput(value);
+  };
+
   return (
     <div className="chat">
       <div className="chat__header">
-        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/> 
-      
-      <div className="chat__headerInfo">
-        <h3>{roomName}</h3>
-        <p>Last seen at...</p>
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+        <div className="chat__headerInfo">
+          <h3>{roomName}</h3>
+          <p>
+            last seen{" "}
+            {new Date(
+              messages[messages.length - 1]?.timestamp?.toDate()
+            ).toUTCString()}
+          </p>
         </div>
-      <div className="chat__headerRight">
-      <IconButton>
-          <SearchOutlined />
-        </IconButton>
-        <IconButton>
-          <AttachFile />
-        </IconButton>
-        <IconButton>
-          <MoreVert />
-        </IconButton>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
         </div>
       </div>
       <div className="chat__body">
-      {messages.map(message => (
-        <p className={`chat__message ${true && "chat__receiver"}`}>
-        <span className="chat__name">{message.name}</span>
-        {message.message}
-        <span className="chat__timestamp">{new Date(message.timestamp?.toDate()).toUTCString()}</span>
-        </p>
-      ))}
-        
+        {messages.map((message) => (
+          <p
+            className={`chat__message ${
+              message.name === user.displayName && "chat__receiver"
+            }`}
+          >
+            <span className="chat__name">{message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">
+              {new Date(message.timestamp?.toDate()).toUTCString()}
+            </span>
+          </p>
+        ))}
       </div>
       <div className="chat__footer">
         <InsertEmoticonIcon />
         <form>
-          <input 
-          value={input} 
-          onChange={e => setInput(e.target.Value)} 
-          placeholder="Type a message" 
-          type="text" />
-          <button onClick={sendMessage}
-          type="submit">Send a message</button>
+          <input
+            value={input}
+            onChange={(event) => handleChange(event)}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
         </form>
         <MicIcon />
       </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
